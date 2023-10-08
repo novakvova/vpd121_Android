f558726493f3a992b795e4258ce3e190df985507
novakvova <novakvova@gmail>
2023-10-08 17:55:25 +0300

    Begin working app

--- Lesson1. Begin/app/build.gradle.kts
+++ Lesson1. Begin/app/build.gradle.kts
@@ -33,6 +33,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // https://mvnrepository.com/artifact/com.github.bumptech.glide/glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Lesson1. Begin/app/src/main/java/com/example/mystore/MainActivity.java
+++ Lesson1. Begin/app/src/main/java/com/example/mystore/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.mystore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "https://vpd121.itstep.click/images/1.jpg";
+
+        ImageView ivAvater = (ImageView) findViewById(R.id.ivAvater);
+
+        Glide.with(this)
+                .load(url)
+                .apply(new RequestOptions().override(600))
+                .into(ivAvater);
+
     }
 }
